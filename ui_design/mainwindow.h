4f272753189f03a0dfdbabde8635ad1c6a67dc6f
chnsheg <1445001103@qq.com>
2024-01-03 17:54:54 +0800

    第一次提交，上传工程

---
+++ ui_design/mainwindow.h
@@ -0,0 +1,57 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include "mytracer.h"
+#include "qcustomplot.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    //曲线部分
+    void InitializePlot(QCustomPlot *CustomPlot); //初始化绘图区
+    void loadTracer(QCustomPlot *CustomPlot);
+    void loadFunctionButton();
+    void loadHomeScreen();
+
+private slots:
+    void plotGraph(QCustomPlot *CustomPlot,
+                   QVector<double> *xtemp,
+                   QVector<double> *ytemp,
+                   int count = 0); //绘制曲线
+    void showTracer(QMouseEvent *event);
+    void start_plot();
+    void clear_plot();
+    void hide_trace();
+
+private:
+    Ui::MainWindow *ui;
+    QCustomPlot *customPlot; //指向当前页面的QCustomPlot
+    QSharedPointer<myTracer> m_TraserX;
+    QSharedPointer<myTracer> m_TracerY;
+    QVector<double> *RF, *laser_y, *L_mc, *L_rc, *L_bc, *Spectrum1, *PMT_Energy, *PMT_Sign,
+        *Fizeau_spectrum, *SpectrumAfterFizeau;
+
+    QVector<QColor> colorContainer = {QColor(226, 60, 255),
+                                      QColor(64, 224, 208),
+                                      QColor(255, 99, 71),
+                                      QColor(255, 255, 0),
+                                      QColor(0, 255, 255),
+                                      QColor(255, 0, 0),
+                                      QColor(0, 255, 0),
+                                      QColor(0, 0, 255),
+                                      QColor(255, 255, 255),
+                                      QColor(0, 0, 0)};
+};
+#endif // MAINWINDOW_H
---
+++ ui_design/mytracer.h
@@ -0,0 +1,42 @@
+#ifndef MYTRACER_H
+#define MYTRACER_H
+
+#include <QObject>
+#include "qcustomplot.h"
+enum TracerType { XAxisTracer, YAxisTracer, DataTracer };
+
+class myTracer : public QObject
+{
+    Q_OBJECT
+public:
+public:
+    explicit myTracer(QCustomPlot *_plot,
+                      QCPGraph *_graph,
+                      TracerType _type); //这里与原贴不同，按照原贴构造总是过不去
+    ~myTracer();
+
+    void setPen(const QPen &pen);
+    void setBrush(const QBrush &brush);
+    void setText(const QString &text, const QString &text1);
+    //void setText(const QString &text);
+    void setLabelPen(const QPen &pen);
+    void updatePosition(double xValue, double yValue);
+
+    void setVisible(bool visible);
+
+protected:
+    QCustomPlot *plot;     //传入实例化的QcustomPlot
+    QCPGraph *graph;       //这里是存传入的绘图图层
+    QCPItemTracer *tracer; // 跟踪的点
+    QCPItemText *label;    // 显示的数值
+    QCPItemLine *arrow;    // 箭头
+
+    TracerType type;
+    bool visible;
+
+signals:
+
+public slots:
+};
+
+#endif // MYTRACER_H
